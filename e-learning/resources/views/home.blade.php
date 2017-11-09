2794e5590068fff8f80914bbda67526f848ddb99
Unknown <tobinzz69@gmail.com>
2017-11-09 15:55:24 +0800

    Homepage
    Homepage stuff and images

---
+++ e-learning/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!-- Extends HTML structure from layouts/app.blade -->
+@extends('layouts.app')
+
+
+    <title>Home</title>
+    @include('inc.banner')
+
+    <div class="container">
+
+
+        <div class="left">
+            @include('inc.imageSlide')
+
+
+        </div>
+    </div>
+
+    </div>
+
