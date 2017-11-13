140e9a6dc2e149bacdcd7bc96a3628780040a03b
Unknown <tobinzz69@gmail.com>
2017-11-13 17:55:50 +0800

    Login field done and Banner non-image

--- e-learning/resources/views/home.blade.php
+++ e-learning/resources/views/home.blade.php
@@ -1,22 +1,22 @@
 <!-- Extends HTML structure from layouts/app.blade -->
 @extends('layouts.app')
+<title>Home</title>
+@include('inc.banner')
+
+<div class="container">
 
 
-    <title>Home</title>
-    @include('inc.banner')
-
-        <div class="container">
-
-        <div class="row">
             <div class="col-md-8 col-lg-8">
                 @include('inc.imageSlide')
 
 
             </div>
 
-            <div class="col-md-4 col-lg-4">
+            <div class="col-md-1 col-lg-4">
+
                 @include('inc.login')
 
             </div>
-    </div>
-           <div/>
+
+
+</div>
--- e-learning/resources/views/inc/banner.blade.php
+++ e-learning/resources/views/inc/banner.blade.php
@@ -1,3 +1,24 @@
-    <div class="banner">
-        <img src="{{URL::asset('img/banner.jpg')}}"height="140" width="1400">
-    </div>
+<div class="banner">
+    <h1 class="banner">Example banner</h1>
+
+
+<style>
+
+    { background: #eee; }
+
+    .banner {
+        left: 0%;
+        display: block;
+        margin: 0px 0px;
+        width: 100%;
+        height: 100px;
+        font: normal 45px/90px 'Verdana';
+        text-align: center;
+        color: black;
+        background: red;
+    }
+
+
+
+</style>
+</div>
