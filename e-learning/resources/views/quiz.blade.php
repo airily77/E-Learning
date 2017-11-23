0744f8ec89c36e91cba552abe7183f3145b7d3be
Unknown <tobinzz69@gmail.com>
2017-11-23 15:09:07 +0800

    Small changes
    Small changes

--- e-learning/resources/views/quiz.blade.php
+++ e-learning/resources/views/quiz.blade.php
@@ -12,9 +12,7 @@
         padding-top: 10%;
         padding-left: 20%;
     }
-    .radio-option{
 
-    }
 
 </style>
 
@@ -28,18 +26,18 @@
        
         <form action="" id="singleanswer">
         <h4><strong>Where is Finland located?</strong></h4>
-            <li><input type="radio" name="answer" value="Answer1"> North America</li> <br>
-            <li><input type="radio" name="answer" value="Answer2"> Asia</li> <br>
-            <li><input type="radio" name="answer" value="Answer3"> Antarctica</li> <br>
-            <li><input type="radio" name="answer" value="Answer4"> Europe</li> <br>
+            <input type="radio" name="answer" value="Answer1"> North America <br>
+            <input type="radio" name="answer" value="Answer2"> Asia <br>
+            <input type="radio" name="answer" value="Answer3"> Antarctica <br>
+            <input type="radio" name="answer" value="Answer4"> Europe <br>
         </form>
 
         <form action="" id="multianswer">
             <h4><strong>Which of these are Chinese cities?</strong></h4>
-            <input type="radio" name="answer0" value="Answer1"> Helsinki <br>
-            <input type="radio" name="answer1" value="Answer2"> Shanghai <br>
-            <input type="radio" name="answer2" value="Answer3"> Stockholm <br>
-            <input type="radio" name="answer3" value="Answer4"> Suzhou <br>
+            <input type="checkbox" name="answer0" value="Answer1"> Helsinki <br>
+            <input type="checkbox" name="answer1" value="Answer2"> Shanghai <br>
+            <input type="checkbox" name="answer2" value="Answer3"> Stockholm <br>
+            <input type="checkbox" name="answer3" value="Answer4"> Suzhou <br>
             
             
         </form>
@@ -52,3 +50,14 @@
 
 </div>
 
+<script>
+
+    var radios = document.getElementsByTagName('input');
+    for(i=0; i<radios.length; i++ ) {
+        radios[i].onclick = function(e) {
+            if(e.ctrlKey) {
+                this.checked = false;
+
+            }
+        }
+</script>
