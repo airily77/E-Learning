a3ff2080eb29c1b884d79ba5dab74791e5426664
Unknown <tobinzz69@gmail.com>
2017-11-24 17:28:56 +0800

    Stuff to specific-course
    Stuff to specific-course

--- e-learning/resources/views/specific-course.blade.php
+++ e-learning/resources/views/specific-course.blade.php
@@ -1,9 +1,28 @@
 <!-- Extends HTML structure from layouts/app.blade -->
 @extends('layouts.app')
 <title>Course <!-- todo pull Course name from DB --></title>
-@include('inc.course.banner2')
 
+<style>
+
+#header{
+    text-align: center;
+    color: black;
+}
+
+
+
+</style>
+
+
+@include('inc.course.banner2')
+<h1 id="header">Course name <!--  //TODO course name from DB  --></h1>
+<h4 id="header">Course description <!-- //TODO Description from DB  --> </h4>
 
 <div class="container">
-    
+    <div class="well">
+
+        @include('inc.specific-course.Videopreview')
+
+
+    </div>
 </div>
