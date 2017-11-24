e2d1f986c2c5b13fd260b64654357488e58214f5
Mattinen Pekka <s1500631@student.businesscollege.fi>
2017-11-24 17:02:52 +0800

    Title from db

--- e-learning/resources/views/specific-course.blade.php
+++ e-learning/resources/views/specific-course.blade.php
@@ -1,9 +1,10 @@
 <!-- Extends HTML structure from layouts/app.blade -->
 @extends('layouts.app')
-<title>Course <!-- todo pull Course name from DB --></title>
+<title>{{$coursedata->title}}</title>
 @include('inc.course.banner2')
 
 
 <div class="container">
-    
+    <p>you are in {{$coursedata->title}}</p>
+    <!-- //TODO Create a button or a div which you can start your exam from. -->
 </div>
